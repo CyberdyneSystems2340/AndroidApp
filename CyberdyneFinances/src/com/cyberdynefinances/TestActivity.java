5a09d790c6423d15e750f9d88a2d79f15ad83016
Dr-Neferious <alchaussee@hotmail.com>
2014-01-31 01:12:34 -0500

    Merge files from M3 to master

--- CyberdyneFinances/src/com/cyberdynefinances/TestActivity.java
+++ CyberdyneFinances/src/com/cyberdynefinances/TestActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.support.v4.app.NavUtils;
 
 public class TestActivity extends Activity {
@@ -11,9 +12,10 @@ public class TestActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_test);
 		// Show the Up button in the action bar.
-		setupActionBar();
+		//setupActionBar();
 	}
 
 	/**
