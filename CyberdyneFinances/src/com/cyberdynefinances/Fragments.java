4a0ae091b6dac38827cde8a2c2964299b9daacc3
Dr-Neferious <alchaussee@hotmail.com>
2014-02-10 12:29:03 -0500

    remove some unused commented out things

--- CyberdyneFinances/src/com/cyberdynefinances/Fragments.java
+++ CyberdyneFinances/src/com/cyberdynefinances/Fragments.java
@@ -27,31 +27,8 @@ public class Fragments
         {
         	//Actual view relating to the fragment needed to gain access to elements of the login screen like text fields
         	root = inflater.inflate(R.layout.activity_login, container, false);
-        	//Button loginButton = (Button) root.findViewById(R.id.loginButton);
-        	//loginButton.setOnClickListener(this);
         	return root;
         }
-
-        //Moved to WelcomeContainer using normal button click method
-        //A button listener click method has to be set in this class rather than the container in order to gain access to elements of the login screen like text fields
-        /*
-		@Override
-		public void onClick(View view) 
-		{
-			EditText editText = (EditText) root.findViewById(R.id.usernameEditText);
-	    	String username = editText.getText().toString();
-	    	editText = (EditText) root.findViewById(R.id.passwordEditText);
-	    	String password = editText.getText().toString();
-	    	if(LoginHandler.isValidLogin(username, password))
-	    	{
-	    		Animation.fade(new Fragments.TestFragment(), getFragmentManager(), R.id.container_welcome);
-	    	}
-	    	else
-	    	{
-	    		Toast.makeText(getActivity(), "Incorrect username or password", Toast.LENGTH_LONG).show();
-	    	}
-		}
-        */
     }
 	
 	//Fragment for the register screen
