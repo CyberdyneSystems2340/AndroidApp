e2636c54e1683a1398e22406ea409d98c82e38c2
R3mak <rmcbride6@gatech.edu>
2014-02-19 14:41:34 -0500

    WIP

--- CyberdyneFinances/src/com/cyberdynefinances/Tester.java
+++ CyberdyneFinances/src/com/cyberdynefinances/Tester.java
@@ -1,5 +1,8 @@
 package com.cyberdynefinances;
 
+import com.cyberdynefinances.DBReaderContract.DBEntry;
+
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.ContentValues;
 
@@ -15,9 +18,22 @@ public class Tester {
 		long newRowId;
 		newRowId = db.insert(
 				DBReaderContract.DBEntry.USER_TABLE_NAME,
-				DBReaderContract.DBEntry.USER_COLUMN_NAME_ID,
+				DBReaderContract.DBEntry.USER_COLUMN_NAME_ACCOUNTS,
 				values);
 		
+		db = dbHelper.getReadableDatabase();
 		
+		String[] projection = {
+				DBEntry.USER_COLUMN_NAME_ID
+		};
+		
+		String sortOrder = DBEntry.USER_COLUMN_NAME_ID + " DESC";
+		String selection = "UserID";
+		String[] selectionArgs = {"TestID"};
+		Cursor c = db.query(DBEntry.USER_TABLE_NAME,
+				projection,selection,selectionArgs, null, null, sortOrder);
+		c.moveToFirst();
+		String itemID = c.getString(c.getColumnIndex(DBEntry.USER_COLUMN_NAME_ID));
+		System.out.println(itemID);
 	}
 }
