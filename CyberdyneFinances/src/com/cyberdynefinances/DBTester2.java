5d00d82db342ebb216843a2022b841ebc020c5af
R3mak <rmcbride6@gatech.edu>
2014-02-20 20:55:44 -0500

    WIP
    Possibly narrowing down the db read/write problems.

--- CyberdyneFinances/src/com/cyberdynefinances/DBTester2.java
+++ CyberdyneFinances/src/com/cyberdynefinances/DBTester2.java
@@ -2,6 +2,7 @@ package com.cyberdynefinances;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
@@ -16,28 +17,9 @@ public class DBTester2 extends Activity {
 		
 		TextView tView = (TextView) findViewById(R.id.dbtester2_textview);
 		AccountDBHelper dbHelper = new AccountDBHelper(MyApplication.getAppContext());
-		SQLiteDatabase db = dbHelper.getReadableDatabase();
-		Cursor c = db.rawQuery("SELECT * FROM " + DBReaderContract.DBEntry.USER_TABLE_NAME, null);
-		String dbName = dbHelper.getDatabaseName();
-		String tableName = DBReaderContract.DBEntry.USER_TABLE_NAME;
 
-		int col0 = c.getColumnIndex("UserID");
-		int col1 = c.getColumnIndex("Password");
-		int col2 = c.getColumnIndex("Accounts");
-		c.moveToLast();
-		String rows = "";
-/*		if (c != null) {
-			do {
-				rows = "UserID: " + c.getString(col0) + ", Pass: " + c.getString(col1) + ", Accounts: " + c.getString(col2); 
-			} while (c.moveToNext());
-		}
-*/
-//		writeToDB(dbHelper);
-//		String itemID = readFromDB(dbHelper);		
-
-		tView.setText("DbName: " + dbName + "\n\nTableName: " + tableName +
-					  "\n\nColumns: " + c.getColumnName(col0) + ", " + c.getColumnName(col1) + ", " + c.getColumnName(col2) +
-					  "\n\nRows: " +rows);
+		writeToDB(dbHelper);
+		tView.setText(readFromDB(dbHelper));
 	}
 
 	@Override
@@ -46,31 +28,54 @@ public class DBTester2 extends Activity {
 		getMenuInflater().inflate(R.menu.dbtester_activity, menu);
 		return true;
 	}
-/*	
+	
 	private void writeToDB(AccountDBHelper dbHelper){
 		SQLiteDatabase db = dbHelper.getWritableDatabase();
 		
 		ContentValues values = new ContentValues();
-		values.put(DBReaderContract.DBEntry.USER_COLUMN_NAME_ID, "TestID");
-		
-		db.insert(DBReaderContract.DBEntry.USER_TABLE_NAME,
-				  DBReaderContract.DBEntry.USER_COLUMN_NAME_ACCOUNTS,
-				  values);
+		String testID = "TestID3";
+		values.put(DBReaderContract.DBEntry.USER_COLUMN_NAME_ID, testID);
+		values.put(DBReaderContract.DBEntry.USER_COLUMN_NAME_PASSWORD, "123456");
+		values.put(DBReaderContract.DBEntry.USER_COLUMN_NAME_ACCOUNTS, "None");
+		SQLiteDatabase db2 = dbHelper.getReadableDatabase();
+		Cursor c = db2.rawQuery("SELECT * FROM " + DBReaderContract.DBEntry.USER_TABLE_NAME, null);
+		boolean userIDTaken = false;/*
+		c.moveToFirst();
+		if (null != c) {
+			for (int i = c.getCount(); i > 0; i--){
+				userIDTaken = c.getString(c.getColumnIndex("UserID")) == testID;
+				c.moveToNext();
+			}
+			if (!userIDTaken) {
+				db.insert(DBReaderContract.DBEntry.USER_TABLE_NAME,
+					  DBReaderContract.DBEntry.USER_COLUMN_NAME_ACCOUNTS,
+					  values);
+			}
+		}*/
+//		((TextView) findViewById(R.id.dbtester2_textview)).setText("Success!");
 	}
 	
 	private String readFromDB(AccountDBHelper dbHelper) {
 		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		Cursor c = db.rawQuery("SELECT * FROM " + DBReaderContract.DBEntry.USER_TABLE_NAME, null);
+		String dbName = dbHelper.getDatabaseName();
+		String tableName = DBReaderContract.DBEntry.USER_TABLE_NAME;
 
-		String[] projection = {DBEntry.USER_COLUMN_NAME_ID};
-		
-		String sortOrder = DBEntry.USER_COLUMN_NAME_ID + " DESC";
-		String selection = "UserID";
-		String[] selectionArgs = {"TestID"};
-		Cursor c = db.query(DBEntry.USER_TABLE_NAME,
-				projection,selection,selectionArgs, null, null, sortOrder);
-		c.moveToFirst();
-		return c.getString(c.getColumnIndex(DBEntry.USER_COLUMN_NAME_ID));
+		int col0 = c.getColumnIndex("UserID");
+		int col1 = c.getColumnIndex("Password");
+		int col2 = c.getColumnIndex("Accounts");
+		String rows = "";
+		if (null != c && 0 != c.getCount()) {
+			c.moveToNext();
+			do{
+				rows += "\nUserID: " + c.getString(col0) + ", Pass: " + c.getString(col1) + ", Accounts: " + c.getString(col2); 
+			} while(c.moveToNext());
+		}
+
+		return "DbName: " + dbName + "\n\nTableName: " + tableName +
+					  "\n\nColumns: " + c.getColumnName(col0) + ", " + c.getColumnName(col1) + ", " + c.getColumnName(col2) +
+					  "\n\nRows: " + rows;
 	}
-*/
+
 
 }
