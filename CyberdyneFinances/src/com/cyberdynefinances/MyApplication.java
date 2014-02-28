00a4dfc585b3cc070984a46cc9ab2463a5162cf5
R3mak <rmcbride6@gatech.edu>
2014-02-28 13:14:08 -0500

    Added file header comments
    Made all dbManagement classes comply with 80 character limit.

--- CyberdyneFinances/src/com/cyberdynefinances/MyApplication.java
+++ CyberdyneFinances/src/com/cyberdynefinances/MyApplication.java
@@ -1,5 +1,12 @@
+/**
+ * This method is needed by the DBHandler and DBHelper classes.
+ * It allows these classes to know which database to grab based on the context.
+ * DO NOT DELETE!!!
+ * 
+ * @author Robert
+ * @version 1.0
+ */
 package com.cyberdynefinances;
-
 import android.app.Application;
 import android.content.Context;
 
