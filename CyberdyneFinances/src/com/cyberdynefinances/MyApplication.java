ce5500f564d7c8aba8116e3618065c8b7d714113
R3mak <rmcbride6@gatech.edu>
2014-03-06 20:19:12 -0500

    MyApplication is checkstyled
    Except for Jdocs

--- CyberdyneFinances/src/com/cyberdynefinances/MyApplication.java
+++ CyberdyneFinances/src/com/cyberdynefinances/MyApplication.java
@@ -7,18 +7,19 @@
  * @version 1.0
  */
 package com.cyberdynefinances;
+
 import android.app.Application;
 import android.content.Context;
 
 public class MyApplication extends Application {
-	private static Context context;
-	
-	public void onCreate() {
-		super.onCreate();
-		MyApplication.context = getApplicationContext();
-	}
-	
-	public static Context getAppContext() {
-		return MyApplication.context;
-	}
+    private static Context context;
+
+    public void onCreate() {
+        super.onCreate();
+        MyApplication.context = getApplicationContext();
+    }
+
+    public static Context getAppContext() {
+        return MyApplication.context;
+    }
 }
