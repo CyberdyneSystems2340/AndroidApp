cb47a10139485b42061fe471bc8fd2aae5dabf39
R3mak <rmcbride6@gatech.edu>
2014-03-06 20:39:46 -0500

    Fixed account_homepage overlap
    Fixed NPE problem in fragements

--- CyberdyneFinances/src/com/cyberdynefinances/Fragments.java
+++ CyberdyneFinances/src/com/cyberdynefinances/Fragments.java
@@ -216,7 +216,7 @@ public class Fragments
                 @Override
                 public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3)
                 {
-                    View view = arg1.getRootView();
+                    //View view = arg1.getRootView();
                     
                 }
 
