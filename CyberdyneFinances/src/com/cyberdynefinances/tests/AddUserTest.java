0eb5e19a41ab8798f75a44abd9df2c7c7dc29686
Dr-Neferious <alchaussee@hotmail.com>
2014-03-30 20:34:47 -0400

    Checkstyled my junit test just in case

--- CyberdyneFinances/src/com/cyberdynefinances/tests/AddUserTest.java
+++ CyberdyneFinances/src/com/cyberdynefinances/tests/AddUserTest.java
@@ -4,21 +4,37 @@ import com.cyberdynefinances.WelcomeContainer;
 import com.cyberdynefinances.dbManagement.DBHandler;
 import android.test.ActivityInstrumentationTestCase2;
 
+/**
+ *Tests adding a username and password to the database.
+ *@author Richard Chaussee
+ */
 public class AddUserTest extends ActivityInstrumentationTestCase2<WelcomeContainer>
 {
+    /**
+     * Test Constructor.
+     */
+    @SuppressWarnings("deprecation")
     public AddUserTest() 
     {
         super("com.cyberdynefinances.WelcomContainer", WelcomeContainer.class);
     }
     
+    /**
+     * Tests that a user can be added and the information is stored in the database.
+     */
     public void testAddUser()
     {
+        //CHECKSTYLE:OFF    suppress error strings occur multiple times in file
         assertTrue(DBHandler.addUser("Test", "pass123"));
+        //CHECKSTYLE:ON
         String[] info = DBHandler.getUserInfo("Test");
         assertEquals("Test", info[0]);
         assertEquals("pass123", info[1]);
     }
     
+    /**
+     * Tests trying to add a user with null parameters.
+     */
     public void testAddNullUser()
     {
         assertFalse(DBHandler.addUser(null, "pass123"));
@@ -26,6 +42,9 @@ public class AddUserTest extends ActivityInstrumentationTestCase2<WelcomeContain
         assertFalse(DBHandler.addUser(null, null));
     }
     
+    /**
+     * Tests trying to add a user with the empty string as parameters.
+     */
     public void testAddEmtpyUser()
     {
         assertFalse(DBHandler.addUser("", "pass123"));
@@ -33,13 +52,18 @@ public class AddUserTest extends ActivityInstrumentationTestCase2<WelcomeContain
         assertFalse(DBHandler.addUser("", ""));
     }
 
+    /**
+     * Tests that multiple users can be added and their information is stored correctly.
+     */
     public void testAddMultipleUsers()
     {
+        //CHECKSTYLE:OFF    suppress error strings occur multiple times in file
         assertTrue(DBHandler.addUser("Test", "pass123"));
         assertTrue(DBHandler.addUser("Test1", "apple"));
         assertTrue(DBHandler.addUser("Test2", "orange"));
         assertTrue(DBHandler.addUser("Test3", "banana"));
         assertTrue(DBHandler.addUser("Test4", "drywall"));
+        //CHECKSTYLE:ON
         String[] info = DBHandler.getUserInfo("Test");
         assertEquals("Test", info[0]);
         assertEquals("pass123", info[1]);
